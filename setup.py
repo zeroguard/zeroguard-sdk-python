a757c06b59370271618e0fa98bb0666ae28fa8bb
ddnomad <dd@ddworks.io>
2019-12-09 23:37:48 +0200

    more fixes to setup.py ported from cli repo

--- setup.py
+++ setup.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
 """ZeroGuard package build script."""
+import json
 import os
-import setuptools
+from setuptools import setup, find_packages
 
 import zeroguard
 
 
-PACKAGE_NAME = 'zeroguard'
+LOCK_FILE = 'Pipfile.lock'
 README_FILE = 'README.md'
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-ABOUT_FILE = os.path.join(BASE_DIR, PACKAGE_NAME, '__version__.py')
+ABOUT_FILE = os.path.join(BASE_DIR, 'zeroguard', '__version__.py')
 
-REQUIREMENTS = []
-TEST_REQUIREMENTS = []
+
+def parse_requirements(lock_fpath):
+    """Parse requirements and test requirements from a given Pipfile.lock.
+
+    :return: Requirements and test requirements usable by setup function.
+    :rtype:  2-tuple of list(str)
+    """
+    with open(lock_fpath) as lock_file:
+        lock_deps = json.load(lock_file)
+
+    return (
+        ['%s%s' % (k, v['version']) for k, v in lock_deps['default'].items()],
+        ['%s%s' % (k, v['version']) for k, v in lock_deps['develop'].items()]
+    )
 
 
 def main():
     """Set up the package."""
+    # Load README.md file content
     with open(README_FILE) as readme_file:
         readme = readme_file.read()
 
-    setuptools.setup(
+    # Parse out requirements from Pipfile.lock
+    requirements, test_requirements = parse_requirements(LOCK_FILE)
+
+    # Setup the package
+    setup(
         name=zeroguard.__title__,
         version=zeroguard.__version__,
         license=zeroguard.__license__,
@@ -34,12 +52,12 @@ def main():
         author_email=zeroguard.__author_email__,
         url=zeroguard.__home_url__,
 
-        packages=[PACKAGE_NAME],
+        packages=find_packages(),
         package_data={'': ['CHANGELOG.md', 'LICENSE']},
 
         python_requires='>=3.5',
-        install_requires=REQUIREMENTS,
-        tests_require=TEST_REQUIREMENTS,
+        install_requires=requirements,
+        tests_require=test_requirements,
 
         classifiers=[
             'Development Status :: 1 - Planning',
